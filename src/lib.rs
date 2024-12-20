489502ec7a58b9404c3fc7b4f3c3a856b4d16e5d
Zhansar <daid8002@gmail.com>
2024-12-20 23:43:28 +0500

    added onkeychange event to change the color

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 extern "C" {
     fn change_color_of_bg(red: f32, green: f32, blue: f32, alpha: f32);
 }
+#[no_mangle]
+pub extern "C" fn key_pressed(){
+    clear_screen_to_color(0.0, 0.0, 1.0, 1.0);
+}
 
 pub fn clear_screen_to_color(red: f32, green: f32, blue: f32, alpha: f32) {
     unsafe {
